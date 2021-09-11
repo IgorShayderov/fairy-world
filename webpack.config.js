059f367df6ece68cd4bd99a35c4a3a18314f65ca
IgorShayderov <flashmob123@gmail.com>
2021-09-11 21:47:16 +0200

    Added sass support

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,23 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  devServer: {
+    static: './dist'
+  },
   entry: './src/index.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader',
+          ]
+      }
+    ]
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
