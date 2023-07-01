1318c69fad8eb04fcc2aa495024d2c81d1491586
Igor <flashmob123@gmail.com>
2023-07-01 17:33:55 +0200

    feat: use redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { I18nextProvider } from 'react-i18next';
+import { Provider } from 'react-redux';
 
 import App from './App.tsx';
 
 import initI18n from './i18n.ts';
+import store from './slices';
 
 import './assets/scss/main.scss';
 
@@ -13,9 +15,11 @@ const initApp = async () => {
 
   ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
       <React.StrictMode>
-        <I18nextProvider i18n={i18n}>
-          <App />
-        </I18nextProvider>
+        <Provider store={store}>
+          <I18nextProvider i18n={i18n}>
+            <App />
+          </I18nextProvider>
+        </Provider>
       </React.StrictMode>,
   );
 };
